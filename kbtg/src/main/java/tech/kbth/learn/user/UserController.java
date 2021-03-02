119f3a028693f70a6293a097fa4b1a590a7f40f9
puttipong.s <puttipong.s@kbtg.tech>
2021-03-02 15:17:43 +0700

    workshop 5

--- kbtg/src/main/java/tech/kbth/learn/user/UserController.java
+++ kbtg/src/main/java/tech/kbth/learn/user/UserController.java
@@ -1,15 +1,15 @@
 package tech.kbth.learn.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
+    @Autowired UserService userService;
     @GetMapping("/user/{id}")
     public UserResponse getUserById(@PathVariable int id) {
-        UserService userService = new UserService();
             return userService.getInfo(id);
     }
 }
